4ae46847de7170d5e320eb571794f81c84c5cd63
UndefinedGrunt <hassler.leonard@live.com>
2022-10-28 18:45:53 +0200

    working on the forms

--- src/components/exercises/CreateExercise.js
+++ src/components/exercises/CreateExercise.js
@@ -1,7 +1,113 @@
-function CreateExercise() {
+
+import { useState } from "react";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
+
+
+
+function CreateExercise(props) {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("");
+  const [intensity, setIntensity] = useState("");
+  const [muscle, setMuscle] = useState("");
+  const [specificArea, setSpecificArea] = useState("");
+  const [duration, setDuration] = useState("");
+  const [timeUnit, setTimeUnit] = useState("");
+  const [imageUrl, setTimageUrl] = useState("");
+  const [description, setDescription] = useState("");
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { title, type, intensity, muscle, specificArea, duration, timeUnit, imageUrl, description };
+
+    const storedToken = localStorage.getItem('authToken');
+
+
+    axios
+      .post(
+        `${API_URL}/api/exercises`,
+        requestBody,
+        { headers: { Authorization: `Bearer ${storedToken}` } }
+      )
+      .then((response) => {
+
+        setTitle("");
+        setCategory("");
+        setType("");
+        setIntensity("");
+        setMuscle("");
+        setSpecificArea("");
+        setDuration("");
+        setTimeUnit("");
+        setTimageUrl("");
+        setDescription("");
+
+
+        props.refreshExercise();
+      })
+      .catch((error) => console.log(error));
+  };
+
+
+
+
+
+
+
   return (
     <>
-      <h5>Thios is a create exercise page</h5>
+      <div>
+        <form onSubmit={handleSubmit} id="exform">
+          <label>Title:</label>
+          <input
+            type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+
+          <label>category:</label>
+          <input
+            type="text" name="category" value={category} onChange={(e) => setCategory(e.target.value)} />
+
+          <label>Choose a type:</label>
+          <input
+            type="text" name="type" value={type} onChange={(e) => setType(e.target.value)} />
+
+          <label>Intensity:</label>
+          <input
+            type="text" name="intensity" value={intensity} onChange={(e) => setIntensity(e.target.value)} />
+
+          <label>muscle:</label>
+          <input
+            type="text" name="muscle" value={muscle} onChange={(e) => setMuscle(e.target.value)} />
+
+          <label>specificArea:</label>
+          <input
+            type="text" name="specificArea" value={specificArea} onChange={(e) => setSpecificArea(e.target.value)} />
+
+          <label>duration:</label>
+          <input
+            type="text" name="duration" value={duration} onChange={(e) => setDuration(e.target.value)} />
+
+          <label>timeUnit:</label>
+          <input
+            type="text" name="timeUnit" value={timeUnit} onChange={(e) => setTimeUnit(e.target.value)} />
+
+          <label>Image:</label>
+          <input
+            type="text" name="imageUrl" value={imageUrl} onChange={(e) => setTimageUrl(e.target.value)} />
+
+          <label>Description:</label>
+          <input
+            type="text" name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+
+          <button type="submit">Create Exercise</button>
+
+
+        </form>
+
+      </div>
     </>
   );
 }
