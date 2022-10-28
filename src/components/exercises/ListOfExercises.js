c23556db59a139dc737b7fe26b3726e84a74aea1
UndefinedGrunt <hassler.leonard@live.com>
2022-10-28 15:40:26 +0200

    corected rendering in list of exercises

--- src/components/exercises/ListOfExercises.js
+++ src/components/exercises/ListOfExercises.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navlink, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Navbar from "../Navbar";
 
 function ListOfExercises() {
   const [exercises, setExercises] = useState([]);
@@ -20,6 +21,18 @@ function ListOfExercises() {
       .catch((err) => console.log(err));
   }, []);
 
+  return (
+    <div>
+      {exercises.map((exercise) => {
+        console.log(exercise);
+        return (
+          <div key={exercise._id}>
+            <h3>{exercise.title}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ListOfExercises;
