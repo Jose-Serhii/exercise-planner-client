ab31a8b155469789c39c380e1e94c1d07e618089
Serhii Kvasha <seregeykvasha@gmail.com>
2022-10-28 17:35:14 +0200

    add functionality to delete exercise

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,33 @@
 import React from "react";
-import { NavLink } from "react-router-dom"
-import "../Navbar.css"
+import { NavLink } from "react-router-dom";
+import "../Navbar.css";
 
 function Navbar() {
-  return <>
-    <div className="navbar">
-      <NavLink to="/login" className="buttonAuth">login</NavLink>
-      |
-      <NavLink to="/signup" className="buttonAuth">signup</NavLink>
-      |
-      <NavLink to="/" className="buttonAuth">Homepage</NavLink>
-      |
-      <NavLink to="/exercises" className="buttonAuth">Exercises</NavLink>
-      |
-      <NavLink to="/create-exercise" className="buttonAuth">Create Exercise</NavLink>
-
-    </div>
-
-
-
-  </>;
+  return (
+    <>
+      <div className="navbar">
+        <NavLink to="/login" className="buttonAuth">
+          login
+        </NavLink>
+        |
+        <NavLink to="/signup" className="buttonAuth">
+          signup
+        </NavLink>
+        |
+        <NavLink to="/" className="buttonAuth">
+          Homepage
+        </NavLink>
+        |
+        <NavLink to="/exercises" className="buttonAuth">
+          Exercises
+        </NavLink>
+        |
+        <NavLink to="/create-exercise" className="buttonAuth">
+          Create Exercise
+        </NavLink>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
--- src/components/exercises/ExerciseDetails.js
+++ src/components/exercises/ExerciseDetails.js
@@ -1,8 +1,53 @@
-function ExerciseDetails() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function ExerciseDetails(props) {
+  const { exerciseId } = useParams();
+
+  const navigate = useNavigate();
+
+  const [exercise, setExercise] = useState({});
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzViYWUzZDU5YTc2NGUwYTVmZGFiZTAiLCJlbWFpbCI6InNlcmVnZXlrdmFzaGFAZ21haWwuY29tIiwibmFtZSI6ImFkbWluaXN0cmF0b3IiLCJpYXQiOjE2NjY5NzExMTMsImV4cCI6MTY2Njk5MjcxM30.fi2NpJEZ9-7hbaS-goTNGBqZCkMbkPY9QXxKigoY9jU";
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5005/api/exercises/${exerciseId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        setExercise(response.data);
+      })
+      .catch((e) => console.log("error getting exercise from API", e));
+  }, []);
+
+  const deleteExercise = () => {
+    axios
+      .delete(`http://localhost:5005/api/exercises/${exerciseId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        navigate("/exercises");
+      })
+      .catch((e) => console.log("error deleting characters", e));
+  };
+
   return (
-    <>
-      <h5>This is an edit exercise page</h5>
-    </>
+    <div>
+      <h3>{exercise.title}</h3>
+      <h3>{exercise.category}</h3>
+      <h3>{exercise.type}</h3>
+      <h3>{exercise.intensity}</h3>
+      <h3>{exercise.muscle}</h3>
+      <h3>{exercise.specificArea}</h3>
+      <h3>{exercise.duration}</h3>
+      <h3>{exercise.timeUnit}</h3>
+      <h3>{exercise.imageUrl}</h3>
+      <h3>{exercise.description}</h3>
+      <button onClick={deleteExercise}>DELETE THIS exercise</button>
+    </div>
   );
 }
 
