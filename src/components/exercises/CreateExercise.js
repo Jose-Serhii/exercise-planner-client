3a38c5b7d969369b9ee448407f6b481180838e75
Serhii Kvasha <seregeykvasha@gmail.com>
2022-10-31 16:39:18 +0100

    minor form changes

--- src/components/exercises/CreateExercise.js
+++ src/components/exercises/CreateExercise.js
@@ -1,11 +1,8 @@
-
-import { useState, } from "react";
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const API_URL = "http://localhost:5005";
-
-
 
 function CreateExercise(props) {
   const navigate = useNavigate();
@@ -21,23 +18,28 @@ function CreateExercise(props) {
   const [imageUrl, setImageUrl] = useState("");
   const [description, setDescription] = useState("");
 
-
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    const requestBody = { title, category, type, intensity, muscle, specificArea, duration, timeUnit, imageUrl, description };
+    const requestBody = {
+      title,
+      category,
+      type,
+      intensity,
+      muscle,
+      specificArea,
+      duration,
+      timeUnit,
+      imageUrl,
+      description,
+    };
 
-    const storedToken = localStorage.getItem('authToken');
-
+    const storedToken = localStorage.getItem("authToken");
 
     axios
-      .post(
-        `${API_URL}/api/exercises`,
-        requestBody,
-        { headers: { Authorization: `Bearer ${storedToken}` } }
-      )
+      .post(`${API_URL}/api/exercises`, requestBody, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
       .then((response) => {
-
         setTitle("");
         setCategory("");
         setType("");
@@ -49,17 +51,9 @@ function CreateExercise(props) {
         setImageUrl("");
         setDescription("");
         navigate("/exercises");
-
-
       })
       .catch((error) => console.log(error));
   };
-
-
-
-
-
-
 
   return (
     <>
@@ -67,49 +61,113 @@ function CreateExercise(props) {
         <form onSubmit={handleSubmit} id="exform">
           <label>Title:</label>
           <input
-            type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
 
-          <label>category:</label>
+          <label>Category:</label>
           <input
-            type="text" name="category" value={category} onChange={(e) => setCategory(e.target.value)} />
+            type="text"
+            name="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
 
           <label>Choose a type:</label>
-          <input
-            type="text" name="type" value={type} onChange={(e) => setType(e.target.value)} />
+          <select
+            type="text"
+            name="type"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option hidden>choose the type...</option>
+            <option>Cardio</option>
+            <option>Strength</option>
+            <option>Balance</option>
+            <option>Flexibility</option>
+            <option>Coordination</option>
+            <option>Warm-up</option>
+            <option>Other</option>
+          </select>
 
           <label>Intensity:</label>
-          <input
-            type="text" name="intensity" value={intensity} onChange={(e) => setIntensity(e.target.value)} />
+          <select
+            type="text"
+            name="intensity"
+            value={intensity}
+            onChange={(e) => setIntensity(e.target.value)}
+          >
+            <option hidden>choose intensity...</option>
+            <option>Low</option>
+            <option>Moderate</option>
+            <option>High</option>
+          </select>
 
-          <label>muscle:</label>
-          <input
-            type="text" name="muscle" value={muscle} onChange={(e) => setMuscle(e.target.value)} />
+          <label>Target muscle:</label>
+          <select
+            type="text"
+            name="muscle"
+            value={muscle}
+            onChange={(e) => setMuscle(e.target.value)}
+          >
+            <option hidden>choose muscle...</option>
+            <option>Chest</option>
+            <option>Back</option>
+            <option>Arms</option>
+            <option>Abdominals</option>
+            <option>Legs</option>
+            <option>Shoulders</option>
+            <option>Other</option>
+          </select>
 
-          <label>specificArea:</label>
+          <label>Specific Area:</label>
           <input
-            type="text" name="specificArea" value={specificArea} onChange={(e) => setSpecificArea(e.target.value)} />
+            type="text"
+            name="specificArea"
+            value={specificArea}
+            onChange={(e) => setSpecificArea(e.target.value)}
+          />
 
-          <label>duration:</label>
+          <label>Duration:</label>
           <input
-            type="text" name="duration" value={duration} onChange={(e) => setDuration(e.target.value)} />
+            type="text"
+            name="duration"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+          />
 
-          <label>timeUnit:</label>
-          <input
-            type="text" name="timeUnit" value={timeUnit} onChange={(e) => setTimeUnit(e.target.value)} />
+          <label>Time Unit:</label>
+          <select
+            type="text"
+            name="timeUnit"
+            value={timeUnit}
+            onChange={(e) => setTimeUnit(e.target.value)}
+          >
+            <option hidden>time unit</option>
+            <option>Minutes</option>
+            <option>Hours</option>
+          </select>
 
           <label>Image:</label>
           <input
-            type="text" name="imageUrl" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+            type="text"
+            name="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
 
           <label>Description:</label>
           <input
-            type="text" name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+            type="text"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
 
-          <button type="submit">Create Exercise</button>
-
-
+          <button type="submit">Add Exercise</button>
         </form>
-
       </div>
     </>
   );
--- src/components/exercises/EditExercise.js
+++ src/components/exercises/EditExercise.js
@@ -1,119 +1,183 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const API_URL = "http://localhost:5005";
 
 function EditExercise(props) {
+  const storedToken = localStorage.getItem("authToken");
 
-    const storedToken = localStorage.getItem('authToken');
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("");
+  const [intensity, setIntensity] = useState("");
+  const [muscle, setMuscle] = useState("");
+  const [specificArea, setSpecificArea] = useState("");
+  const [duration, setDuration] = useState("");
+  const [timeUnit, setTimeUnit] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [description, setDescription] = useState("");
 
+  const { exerciseId } = useParams();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5005/api/exercises/${exerciseId}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        const oneExercise = response.data;
+        setTitle(oneExercise.title);
+        setCategory(oneExercise.category);
+        setType(oneExercise.type);
+        setIntensity(oneExercise.intensity);
+        setMuscle(oneExercise.muscle);
+        setSpecificArea(oneExercise.specificArea);
+        setDuration(oneExercise.duration);
+        setTimeUnit(oneExercise.timeUnit);
+        setImageUrl(oneExercise.imageUrl);
+        setDescription(oneExercise.description);
+      })
+      .catch((error) => console.log(error));
+  }, [exerciseId]);
 
-    const [title, setTitle] = useState("");
-    const [category, setCategory] = useState("");
-    const [type, setType] = useState("");
-    const [intensity, setIntensity] = useState("");
-    const [muscle, setMuscle] = useState("");
-    const [specificArea, setSpecificArea] = useState("");
-    const [duration, setDuration] = useState("");
-    const [timeUnit, setTimeUnit] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
-    const [description, setDescription] = useState("");
-
-
-    const { exerciseId } = useParams();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        axios
-            .get(`http://localhost:5005/api/exercises/${exerciseId}`, {
-                headers: { Authorization: `Bearer ${storedToken}` },
-            })
-            .then((response) => {
-                const oneExercise = response.data;
-                setTitle(oneExercise.title);
-                setCategory(oneExercise.category);
-                setType(oneExercise.type);
-                setIntensity(oneExercise.intensity);
-                setMuscle(oneExercise.muscle);
-                setSpecificArea(oneExercise.specificArea);
-                setDuration(oneExercise.duration);
-                setTimeUnit(oneExercise.timeUnit);
-                setImageUrl(oneExercise.imageUrl);
-                setDescription(oneExercise.description);
-
-            })
-            .catch((error) => console.log(error));
-
-    }, [exerciseId]);
-
-
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        const requestBody = { title, category, type, intensity, muscle, specificArea, duration, timeUnit, imageUrl, description };
-
-        axios
-            .put(`${API_URL}/api/exercises/${exerciseId}`, requestBody, {
-                headers: { Authorization: `Bearer ${storedToken}` }
-            })
-            .then((response) => {
-                navigate(`/exercises/${exerciseId}`)
-            });
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = {
+      title,
+      category,
+      type,
+      intensity,
+      muscle,
+      specificArea,
+      duration,
+      timeUnit,
+      imageUrl,
+      description,
     };
 
+    axios
+      .put(`${API_URL}/api/exercises/${exerciseId}`, requestBody, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        navigate(`/exercises/${exerciseId}`);
+      });
+  };
 
+  return (
+    <div className="EditExercisePage">
+      <h3>Edit the Project</h3>
 
-    return (
-        <div className="EditExercisePage">
-            <h3>Edit the Project</h3>
+      <form onSubmit={handleFormSubmit} id="exform">
+        <label>Title:</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-            <form onSubmit={handleFormSubmit} id="exform">
-                <label>Title:</label>
-                <input
-                    type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <label>Category:</label>
+        <input
+          type="text"
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
 
-                <label>category:</label>
-                <input
-                    type="text" name="category" value={category} onChange={(e) => setCategory(e.target.value)} />
+        <label>Choose a type:</label>
+        <select
+          type="text"
+          name="type"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option>Cardio</option>
+          <option>Strength</option>
+          <option>Balance</option>
+          <option>Flexibility</option>
+          <option>Coordination</option>
+          <option>Warm - up</option>
+          <option>Other</option>
+        </select>
 
-                <label>Choose a type:</label>
-                <input
-                    type="text" name="type" value={type} onChange={(e) => setType(e.target.value)} />
+        <label>Intensity:</label>
+        <select
+          type="text"
+          name="intensity"
+          value={intensity}
+          onChange={(e) => setIntensity(e.target.value)}
+        >
+          <option>Low</option>
+          <option>Moderate</option>
+          <option>High</option>
+        </select>
 
-                <label>Intensity:</label>
-                <input
-                    type="text" name="intensity" value={intensity} onChange={(e) => setIntensity(e.target.value)} />
+        <label>Target muscle:</label>
+        <select
+          type="text"
+          name="muscle"
+          value={muscle}
+          onChange={(e) => setMuscle(e.target.value)}
+        >
+          <option>Chest</option>
+          <option>Back</option>
+          <option>Arms</option>
+          <option>Abdominals</option>
+          <option>Legs</option>
+          <option>Shoulders</option>
+          <option>Other</option>
+        </select>
 
-                <label>muscle:</label>
-                <input
-                    type="text" name="muscle" value={muscle} onChange={(e) => setMuscle(e.target.value)} />
+        <label>Specific Area:</label>
+        <input
+          type="text"
+          name="specificArea"
+          value={specificArea}
+          onChange={(e) => setSpecificArea(e.target.value)}
+        />
 
-                <label>specificArea:</label>
-                <input
-                    type="text" name="specificArea" value={specificArea} onChange={(e) => setSpecificArea(e.target.value)} />
+        <label>Duration:</label>
+        <input
+          type="text"
+          name="duration"
+          value={duration}
+          onChange={(e) => setDuration(e.target.value)}
+        />
 
-                <label>duration:</label>
-                <input
-                    type="text" name="duration" value={duration} onChange={(e) => setDuration(e.target.value)} />
+        <label>Time Unit:</label>
+        <select
+          type="text"
+          name="timeUnit"
+          value={timeUnit}
+          onChange={(e) => setTimeUnit(e.target.value)}
+        >
+          <option>Minutes</option>
+          <option>Hours</option>
+        </select>
 
-                <label>timeUnit:</label>
-                <input
-                    type="text" name="timeUnit" value={timeUnit} onChange={(e) => setTimeUnit(e.target.value)} />
+        <label>Image:</label>
+        <input
+          type="text"
+          name="imageUrl"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
 
-                <label>Image:</label>
-                <input
-                    type="text" name="imageUrl" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+        <label>Description:</label>
+        <input
+          type="text"
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-                <label>Description:</label>
-                <input
-                    type="text" name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
-
-                <button type="submit">Edit Exercise</button>
-            </form>
-
-        </div>
-    );
+        <button type="submit">Save Changes</button>
+      </form>
+    </div>
+  );
 }
 
 export default EditExercise;
